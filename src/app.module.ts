a9ca7011ee79f3d36d85fbec7415c6eae0900fc1
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-04-02 23:53:00 +0600

    esModuleInterop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(config.get('mongoDbUrl'), {
+      useNewUrlParser: true,
+      keepAlive: true,
+      useUnifiedTopology: true,
+      w: 1,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
